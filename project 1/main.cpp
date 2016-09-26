bc040c679b9eaf4e500d2a5459692d955c10ab45
ikaikastine <ikaikastine@gmail.com>
2016-09-26 14:40:33 -0700

    added project 1 framework

---
+++ project 1/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#endif
+
+
+
+void InitGraphics( );
+
+int main(int argc, char * argv[]) {
+    
+    glutInit( &argc, argv );
+    
+    //InitGraphics( );
+    
+}
