4a046f3cf64db6c07cf9177057b8fa911e1ec4e6
Kevin Stine <ikaikastine@gmail.com>
2016-12-02 17:33:25 -0800

    .DS_Store banished forever :fire:

---
+++ final-project/final-project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  final-project
+//
+//  Created by Kevin Stine on 12/2/16.
+//  Copyright © 2016 Kevin Stine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
