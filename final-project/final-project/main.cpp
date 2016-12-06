6b70acada3a293e21195559a6bc3c911794697ab
Kevin Stine <ikaikastine@gmail.com>
2016-12-05 22:45:23 -0800

    added in x-wing

--- final-project/final-project/main.cpp
+++ final-project/final-project/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include <ctype.h>
+
+#include <vector>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -59,7 +63,33 @@ const char *GLUITITLE   = { "User Interface Window" };
 #define BLADE_WIDTH     0.4
 
 
+// delimiters for parsing the obj file:
 
+#define OBJDELIMS		" \t"
+
+
+struct Vertex
+{
+    float x, y, z;
+};
+
+
+struct Normal
+{
+    float nx, ny, nz;
+};
+
+
+struct TextureCoord
+{
+    float s, t, p;
+};
+
+
+struct face
+{
+    int v, n, t;
+};
 
 // what the glui package defines as true and false:
 
@@ -275,6 +305,7 @@ int     WhichTex;
 GLuint  tex0, tex1, textureBind;
 int     textureView;
 GLuint  DeathStar;
+GLuint  XWing;
 
 
 // function prototypes:
@@ -306,6 +337,11 @@ void	Axes( float );
 void	HsvRgb( float[3], float [3] );
 void    Cross( float[3], float[3], float[3] );
 float   Unit( float[3], float[3] );
+
+void	Cross( float [3], float [3], float [3] );
+char *	ReadRestOfLine( FILE * );
+void	ReadObjVTN( char *, int *, int *, int * );
+int     LoadObjFile( char * );
 
 void    MjbSphere( float, int, int );
 //unsigned char * BmpToTexture( char, int, int );
@@ -499,19 +535,20 @@ Display( )
     // since we are using glScalef( ), be sure normals get unitized:
     
     glEnable( GL_NORMALIZE );
-    /*
+    
     glPushMatrix();
-    glTranslatef(-10.0, 0., 0.);
-    glCallList( DeathStar );
+    glScalef(0.25, 0.25, 0.25);
+    glTranslatef(0., 5., 0.);
+    glCallList( XWing );
     glPopMatrix();
-    */
+    
     // Draw planet and translate it it +5.0 in the x direction
     glPushMatrix();
         glTranslatef(-10.0, 0., 0.);
         glCallList( DeathStar );
         //glTranslatef(10.0, 0., 0.);
     
-        glTranslatef(10.0, 0., 0.);
+        glTranslatef(10., 0., 0.);
         glEnable(GL_TEXTURE_2D);
         glMatrixMode( GL_TEXTURE );
         glLoadIdentity();
@@ -519,7 +556,7 @@ Display( )
         glCallList( BoxList );
         glDisable(GL_TEXTURE_2D);
     
-    glTranslatef(-10.0, 0., 0.);
+    //glTranslatef(-10.0, 0., 0.);
     glPopMatrix();
     
     
@@ -837,6 +874,11 @@ InitLists( )
     MjbSphere(5, 2000, 2000);
     glPopMatrix();
     glEndList();
+    
+    XWing = glGenLists( 1 );
+    glNewList(XWing, GL_COMPILE);
+    LoadObjFile( "x-wing.obj" );
+    glEndList( );
     
     char    *WorldTex = (char *) "world.bmp";
     int     height;
@@ -1555,3 +1597,360 @@ ReadShort( FILE *fp )
     b1 = fgetc( fp );
     return ( b1 << 8 )  |  b0;
 }
+
+int
+LoadObjFile( char *name )
+{
+    char *cmd;		// the command string
+    char *str;		// argument string
+    
+    std::vector <struct Vertex> Vertices(10000);
+    std::vector <struct Normal> Normals(10000);
+    std::vector <struct TextureCoord> TextureCoords(10000);
+    
+    Vertices.clear();
+    Normals.clear();
+    TextureCoords.clear();
+    
+    struct Vertex sv;
+    struct Normal sn;
+    struct TextureCoord st;
+    
+    
+    // open the input file:
+    
+    FILE *fp = fopen( name, "r" );
+    if( fp == NULL )
+    {
+        fprintf( stderr, "Cannot open .obj file '%s'\n", name );
+        return 1;
+    }
+    
+    
+    float xmin = 1.e+37f;
+    float ymin = 1.e+37f;
+    float zmin = 1.e+37f;
+    float xmax = -xmin;
+    float ymax = -ymin;
+    float zmax = -zmin;
+    
+    glBegin( GL_TRIANGLES );
+    
+    for( ; ; )
+    {
+        char *line = ReadRestOfLine( fp );
+        if( line == NULL )
+            break;
+        
+        
+        // skip this line if it is a comment:
+        
+        if( line[0] == '#' )
+            continue;
+        
+        
+        // skip this line if it is something we don't feel like handling today:
+        
+        if( line[0] == 'g' )
+            continue;
+        
+        if( line[0] == 'm' )
+            continue;
+        
+        if( line[0] == 's' )
+            continue;
+        
+        if( line[0] == 'u' )
+            continue;
+        
+        
+        // get the command string:
+        
+        cmd = strtok( line, OBJDELIMS );
+        
+        
+        // skip this line if it is empty:
+        
+        if( cmd == NULL )
+            continue;
+        
+        
+        if( strcmp( cmd, "v" )  ==  0 )
+        {
+            str = strtok( NULL, OBJDELIMS );
+            sv.x = atof(str);
+            
+            str = strtok( NULL, OBJDELIMS );
+            sv.y = atof(str);
+            
+            str = strtok( NULL, OBJDELIMS );
+            sv.z = atof(str);
+            
+            Vertices.push_back( sv );
+            
+            if( sv.x < xmin )	xmin = sv.x;
+            if( sv.x > xmax )	xmax = sv.x;
+            if( sv.y < ymin )	ymin = sv.y;
+            if( sv.y > ymax )	ymax = sv.y;
+            if( sv.z < zmin )	zmin = sv.z;
+            if( sv.z > zmax )	zmax = sv.z;
+            
+            continue;
+        }
+        
+        
+        if( strcmp( cmd, "vn" )  ==  0 )
+        {
+            str = strtok( NULL, OBJDELIMS );
+            sn.nx = atof( str );
+            
+            str = strtok( NULL, OBJDELIMS );
+            sn.ny = atof( str );
+            
+            str = strtok( NULL, OBJDELIMS );
+            sn.nz = atof( str );
+            
+            Normals.push_back( sn );
+            
+            continue;
+        }
+        
+        
+        if( strcmp( cmd, "vt" )  ==  0 )
+        {
+            st.s = st.t = st.p = 0.;
+            
+            str = strtok( NULL, OBJDELIMS );
+            st.s = atof( str );
+            
+            str = strtok( NULL, OBJDELIMS );
+            if( str != NULL )
+                st.t = atof( str );
+            
+            str = strtok( NULL, OBJDELIMS );
+            if( str != NULL )
+                st.p = atof( str );
+            
+            TextureCoords.push_back( st );
+            
+            continue;
+        }
+        
+        
+        if( strcmp( cmd, "f" )  ==  0 )
+        {
+            struct face vertices[10];
+            for( int i = 0; i < 10; i++ )
+            {
+                vertices[i].v = 0;
+                vertices[i].n = 0;
+                vertices[i].t = 0;
+            }
+            
+            int sizev = (int)Vertices.size();
+            int sizen = (int)Normals.size();
+            int sizet = (int)TextureCoords.size();
+            
+            int numVertices = 0;
+            bool valid = true;
+            int vtx = 0;
+            char *str;
+            while( ( str = strtok( NULL, OBJDELIMS ) )  !=  NULL )
+            {
+                int v, n, t;
+                ReadObjVTN( str, &v, &t, &n );
+                
+                // if v, n, or t are negative, they are wrt the end of their respective list:
+                
+                if( v < 0 )
+                    v += ( sizev + 1 );
+                
+                if( n < 0 )
+                    n += ( sizen + 1 );
+                
+                if( t < 0 )
+                    t += ( sizet + 1 );
+                
+                
+                // be sure we are not out-of-bounds (<vector> will abort):
+                
+                if( t > sizet )
+                {
+                    if( t != 0 )
+                        fprintf( stderr, "Read texture coord %d, but only have %d so far\n", t, sizet );
+                    t = 0;
+                }
+                
+                if( n > sizen )
+                {
+                    if( n != 0 )
+                        fprintf( stderr, "Read normal %d, but only have %d so far\n", n, sizen );
+                    n = 0;
+                }
+                
+                if( v > sizev )
+                {
+                    if( v != 0 )
+                        fprintf( stderr, "Read vertex coord %d, but only have %d so far\n", v, sizev );
+                    v = 0;
+                    valid = false;
+                }
+                
+                vertices[vtx].v = v;
+                vertices[vtx].n = n;
+                vertices[vtx].t = t;
+                vtx++;
+                
+                if( vtx >= 10 )
+                    break;
+                
+                numVertices++;
+            }
+            
+            
+            // if vertices are invalid, don't draw anything this time:
+            
+            if( ! valid )
+                continue;
+            
+            if( numVertices < 3 )
+                continue;
+            
+            
+            // list the vertices:
+            
+            int numTriangles = numVertices - 2;
+            
+            for( int it = 0; it < numTriangles; it++ )
+            {
+                int vv[3];
+                vv[0] = 0;
+                vv[1] = it + 1;
+                vv[2] = it + 2;
+                
+                // get the planar normal, in case vertex normals are not defined:
+                
+                struct Vertex *v0 = &Vertices[ vertices[ vv[0] ].v - 1 ];
+                struct Vertex *v1 = &Vertices[ vertices[ vv[1] ].v - 1 ];
+                struct Vertex *v2 = &Vertices[ vertices[ vv[2] ].v - 1 ];
+                
+                float v01[3], v02[3], norm[3];
+                v01[0] = v1->x - v0->x;
+                v01[1] = v1->y - v0->y;
+                v01[2] = v1->z - v0->z;
+                v02[0] = v2->x - v0->x;
+                v02[1] = v2->y - v0->y;
+                v02[2] = v2->z - v0->z;
+                Cross( v01, v02, norm );
+                Unit( norm, norm );
+                glNormal3fv( norm );
+                
+                for( int vtx = 0; vtx < 3 ; vtx++ )
+                {
+                    if( vertices[ vv[vtx] ].t != 0 )
+                    {
+                        struct TextureCoord *tp = &TextureCoords[ vertices[ vv[vtx] ].t - 1 ];
+                        glTexCoord2f( tp->s, tp->t );
+                    }
+                    
+                    if( vertices[ vv[vtx] ].n != 0 )
+                    {
+                        struct Normal *np = &Normals[ vertices[ vv[vtx] ].n - 1 ];
+                        glNormal3f( np->nx, np->ny, np->nz );
+                    }
+                    
+                    struct Vertex *vp = &Vertices[ vertices[ vv[vtx] ].v - 1 ];
+                    glVertex3f( vp->x, vp->y, vp->z );
+                }
+            }
+            continue;
+        }
+        
+        
+        if( strcmp( cmd, "s" )  ==  0 )
+        {
+            continue;
+        }
+        
+    }
+    
+    glEnd();
+    fclose( fp );
+    
+    fprintf( stderr, "Obj file range: [%8.3f,%8.3f,%8.3f] -> [%8.3f,%8.3f,%8.3f]\n",
+            xmin, ymin, zmin,  xmax, ymax, zmax );
+    fprintf( stderr, "Obj file center = (%8.3f,%8.3f,%8.3f)\n",
+            (xmin+xmax)/2., (ymin+ymax)/2., (zmin+zmax)/2. );
+    fprintf( stderr, "Obj file  span = (%8.3f,%8.3f,%8.3f)\n",
+            xmax-xmin, ymax-ymin, zmax-zmin );
+    
+    return 0;
+}
+
+char *
+ReadRestOfLine( FILE *fp )
+{
+    static char *line;
+    std::vector<char> tmp(1000);
+    tmp.clear();
+    
+    for( ; ; )
+    {
+        int c = getc( fp );
+        
+        if( c == EOF  &&  tmp.size() == 0 )
+        {
+            return NULL;
+        }
+        
+        if( c == EOF  ||  c == '\n' )
+        {
+            delete [] line;
+            line = new char [ tmp.size()+1 ];
+            for( int i = 0; i < (int)tmp.size(); i++ )
+            {
+                line[i] = tmp[i];
+            }
+            line[ tmp.size() ] = '\0';	// terminating null
+            return line;
+        }
+        else
+        {
+            tmp.push_back( c );
+        }
+    }
+    
+    return "";
+}
+
+
+void
+ReadObjVTN( char *str, int *v, int *t, int *n )
+{
+    // can be one of v, v//n, v/t, v/t/n:
+    
+    if( strstr( str, "//") )				// v//n
+    {
+        *t = 0;
+        sscanf( str, "%d//%d", v, n );
+        return;
+    }
+    else if( sscanf( str, "%d/%d/%d", v, t, n ) == 3 )	// v/t/n
+    {
+        return;
+    }
+    else
+    {
+        *n = 0;
+        if( sscanf( str, "%d/%d", v, t ) == 2 )		// v/t
+        {
+            return;
+        }
+        else						// v
+        {
+            *n = *t = 0;
+            sscanf( str, "%d", v );
+        }
+    }
+}
+
